7af4cbb17ac0b0d295c1b7ca55c5b118f5756aaa
karlines <karlines@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-06-19 12:45:46 +0000

    boundary value solvers added
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rootsolve@44 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/multiroot.R
+++ pkg/R/multiroot.R
@@ -1,10 +1,58 @@
+## =============================================================================
+## multiroot.1D: root of multiple nonlinear equations
+## resulting by discretizing (partial) differential equations
+## =============================================================================
+
+multiroot.1D <- function (f, start, maxiter=100,
+       rtol=1e-6, atol=1e-8, ctol=1e-8, nspec = NULL,
+       dimens = NULL, verbose=FALSE, positive=FALSE, names=NULL, ...)  {
+  N        <- length(start)
+  if (!is.numeric(start))
+    stop("start conditions should be numeric")
+  if (!is.numeric(maxiter))
+    stop("`maxiter' must be numeric")
+  if (as.integer(maxiter) < 1)
+    stop ("maxiter must be >=1")
+  if (!is.numeric(rtol))
+    stop("`rtol' must be numeric")
+  if (!is.numeric(atol))
+    stop("`atol' must be numeric")
+  if (!is.numeric(ctol))
+    stop("`ctol' must be numeric")
+  if (length(atol) > 1 && length(atol) != N)
+    stop("`atol' must either be a scalar, or as long as `start'")
+  if (length(rtol) > 1 && length(rtol) != N)
+    stop("`rtol' must either be a scalar, or as long as `y'")
+  if (length(ctol) > 1)
+    stop("`ctol' must be a scalar")
+
+    Fun <- function (time=0,x,parms=NULL)
+      list(f(x,...))
+
+    x <- steady.1D(y=start,time=0,func=Fun,parms=NULL,atol=atol,
+              rtol=rtol,ctol=ctol,positive=positive,method="stode",
+              nspec=nspec,dimens=dimens,names=names,cyclicBnd=NULL)
+    precis <- attr(x,"precis")
+    attributes(x)<-NULL
+
+    x        <- unlist(x)
+    reffx    <-f(x,...)
+    i        <- length(precis)
+
+  names(x) <- names(start)
+
+  return(list(root=x,f.root=reffx,iter=i,estim.precis=precis[length(precis)]))
+
+}
 
 ## =============================================================================
 ## multiroot: root of multiple simultaneous nonlinear equations
 ## =============================================================================
 
-multiroot <- function(f, start, maxiter=100, rtol=1e-6, atol=1e-8, ctol=1e-8,        # minimal change in dy
-                      useFortran=TRUE, positive=FALSE, ...)  {
+multiroot <- function(f, start, maxiter=100, rtol=1e-6, atol=1e-8, ctol=1e-8,
+                      useFortran=TRUE,positive=FALSE,
+                      jacfunc=NULL, jactype="fullint", verbose=FALSE,
+                      bandup=1, banddown=1, ...)  {
 
   N        <- length(start)
   if (!is.numeric(start))
@@ -29,9 +77,21 @@ multiroot <- function(f, start, maxiter=100, rtol=1e-6, atol=1e-8, ctol=1e-8,   
   if (useFortran) {
     Fun <- function (time=0,x,parms=NULL)
       list(f(x,...))
- 
-    x <- steady(y=start,time=0,func=Fun,parms=NULL,atol=atol,positive=positive,
-              rtol=rtol,ctol=ctol,jacfunc="fullint",maxiter=maxiter)
+    JacFunc <- jacfunc
+    if (!is.null (jacfunc))
+      JacFunc <-  function (time=0,x,parms=NULL)
+         jacfunc(x,...)
+    method <- "stode"
+    if (jactype=="sparse") {
+      method <- "stodes"
+      if (! is.null (jacfunc)) stop("jacfunc can not be used when jactype='sparse'")
+      x <- stodes(y=start,time=0,func=Fun,parms=NULL,atol=atol,positive=positive,
+              rtol=rtol,ctol=ctol,maxiter=maxiter, verbose=verbose)
+    }else
+      x <- steady(y=start,time=0,func=Fun,parms=NULL,atol=atol,positive=positive,
+              rtol=rtol,ctol=ctol,maxiter=maxiter,method=method,
+              jacfunc=JacFunc,jactype=jactype,verbose=verbose,bandup=bandup,
+              banddown=banddown)
     precis <- attr(x,"precis")
     attributes(x)<-NULL
 
@@ -39,7 +99,7 @@ multiroot <- function(f, start, maxiter=100, rtol=1e-6, atol=1e-8, ctol=1e-8,   
     reffx    <-f(x,...)
     i        <- length(precis)
     
-  } else {  # simple R-implementation
+  } else {  # simple R-implementation - ignores the Jacobian settings
 
     precis   <- NULL
 
